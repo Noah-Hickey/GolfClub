ad75c337d7a518d0e4f638add2e6c237c08cfb36
Noah-Hickey <Noah.Hickey@keyin.com>
2025-07-24 14:18:56 -0230

    Created the project structure.

---
+++ GolfClub/src/main/java/com/golfclub/golfclub/GolfClubApplication.java
@@ -0,0 +1,13 @@
+package com.golfclub.golfclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfClubApplication.class, args);
+	}
+
+}
